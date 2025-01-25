1e3040fee98ff1ae79ab1f4ca7bbe672ff3c03b7
gadoua50@gmail.com <gadoua50@gmail.com>
2025-01-25 16:05:30 +0200

    finish security, static files and public folder

--- public/index.php
+++ public/index.php
@@ -6,11 +6,11 @@ use Framework\Dispatcher;
 use Framework\Dotenv;
 
 spl_autoload_register(function ($className) {
-    require_once __DIR__ . '/src/' . ltrim(str_replace('\\', '/', $className), '\\') . '.php';
+    require_once __DIR__ . '/../src/' . ltrim(str_replace('\\', '/', $className), '\\') . '.php';
 });
 
 $dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/.env');
+$dotenv->load(__DIR__ . '/../.env');
 
 set_error_handler('Framework\ErrorHandler::handleError');
 set_exception_handler('Framework\ErrorHandler::handleException');
@@ -20,9 +20,9 @@ if ($path === false) {
     throw new UnexpectedValueException("Malformed URL: '{$_SERVER['REQUEST_URI']}'", 500);
 }
 
-$router = require_once __DIR__ . '/config/route.php';
+$router = require_once __DIR__ . '/../config/route.php';
 
-$container = require_once __DIR__ . '/config/services.php';
+$container = require_once __DIR__ . '/../config/services.php';
 
 // Dispatching
 $dispatcher = new Dispatcher($router, $container);
